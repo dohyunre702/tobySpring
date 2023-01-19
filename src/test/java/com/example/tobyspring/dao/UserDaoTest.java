944ff9558689cded657160214071ffeb7dfc812f
DHLee <dohyunre702@gmail.com>
2023-01-19 22:52:13 +0900

    [add] TestCode에 ApplicationContext 적용

--- src/test/java/com/example/tobyspring/dao/UserDaoTest.java
+++ src/test/java/com/example/tobyspring/dao/UserDaoTest.java
@@ -3,12 +3,23 @@ package com.example.tobyspring.dao;
 import com.example.tobyspring.factory.DaoFactory;
 import com.example.tobyspring.user.User;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+//테스트코드에서 Spring ApplicationContext 사용 위해 @ExtendWith, @ContextConfiguration 추가
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = DaoFactory.class)
 class UserDaoTest {
+
+    @Autowired
+    ApplicationContext applicationContext;
 
     @Test
     void addAndGet() throws SQLException, ClassNotFoundException {
