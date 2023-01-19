68d562d146dd735eac36add1de9bf7530aec8f79
DHLee <dohyunre702@gmail.com>
2023-01-19 22:50:39 +0900

    [add] Spring Bean 도입

--- src/main/java/com/example/tobyspring/factory/DaoFactory.java
+++ src/main/java/com/example/tobyspring/factory/DaoFactory.java
@@ -3,18 +3,22 @@ package com.example.tobyspring.factory;
 import com.example.tobyspring.ConnectionMaker;
 import com.example.tobyspring.LocalConnectionMaker;
 import com.example.tobyspring.dao.UserDao;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.sql.Connection;
 
-
+@Configuration
 public class DaoFactory {
 
     //UserDao의 생성 책임을 맡음
+    @Bean
     public UserDao userDao() {
         UserDao userDao = new UserDao(connectionMaker());
         return userDao;
     }
 
+    @Bean
     public ConnectionMaker connectionMaker() {
         return new LocalConnectionMaker();
     }
